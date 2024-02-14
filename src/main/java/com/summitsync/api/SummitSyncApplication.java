a468c07dade4997dca6c535eccf140735935f4de
Marius Meschter <marius.meschter@univention.de>
2024-02-14 11:13:28 +0100

    initial commit

---
+++ src/main/java/com/summitsync/api/SummitSyncApplication.java
@@ -0,0 +1,13 @@
+package com.summitsync.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummitSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummitSyncApplication.class, args);
+	}
+
+}
