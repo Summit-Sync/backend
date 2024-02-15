7e43cc0ccb27a2304c624c380b96317fffa2a61a
Marius Meschter <marius.meschter@univention.de>
2024-02-15 10:58:54 +0100

    configure SpringSecurity
    for now just allow everything
    
    JRA-39

---
+++ src/main/java/com/summitsync/api/WebSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.summitsync.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests.anyRequest().permitAll())
+                .sessionManagement((man) -> man.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+}
