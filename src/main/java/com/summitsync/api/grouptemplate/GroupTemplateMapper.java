7b81ce5034cab69ffe285ae61e4e425f5a4f0e68
Jan-Okke <jan1263@schule.bremen.de>
2024-02-21 11:19:06 +0100

    GroupTemplateCRUD

---
+++ src/main/java/com/summitsync/api/grouptemplate/GroupTemplateMapper.java
@@ -0,0 +1,25 @@
+package com.summitsync.api.grouptemplate;
+
+public class GroupTemplateMapper {
+
+    public GroupTemplate mapDtoToTemplate(GroupTemplateDTO dto) {
+        GroupTemplate template = new GroupTemplate();
+        template.setTrainerKey(dto.getTrainerKey());
+        template.setAcronym(dto.getAcronym());
+        template.setPricePerTrainerPerHour(dto.getPricePerTrainerPerHour());
+        template.setDescription(dto.getDescription());
+        template.setRequiredQualifications(dto.getRequiredQualification());
+        return template;
+    }
+
+    public GroupTemplateDTO mapTemplateToDto(GroupTemplate template) {
+        GroupTemplateDTO dto = new GroupTemplateDTO();
+        dto.setTrainerKey(template.getTrainerKey());
+        dto.setAcronym(template.getAcronym());
+        dto.setPricePerTrainerPerHour(template.getPricePerTrainerPerHour());
+        dto.setDescription(template.getDescription());
+        dto.setRequiredQualification(template.getRequiredQualifications());
+        return dto;
+    }
+
+}
