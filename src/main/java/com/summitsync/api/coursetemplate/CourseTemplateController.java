473f9343f8c569d65cd47c14016dae661b1e6ca0
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-02-21 14:51:16 +0100

    SUMMIT-59 added REST-endpoint

--- src/main/java/com/summitsync/api/coursetemplate/CourseTemplateController.java
+++ src/main/java/com/summitsync/api/coursetemplate/CourseTemplateController.java
@@ -1,15 +1,29 @@
 package com.summitsync.api.coursetemplate;
 
+import com.summitsync.api.coursetemplate.dto.CourseTemplateDto;
+import com.summitsync.api.coursetemplate.dto.PostCourseTemplateDto;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/coursetemplate")
+@RestController
+@RequestMapping("/api/v1/coursetemplate")
+@RequiredArgsConstructor
 public class CourseTemplateController {
 
+    private final CourseTemplateService service;
+    private final CourseTemplateMappingService courseTemplateMappingService;
+
     @PostMapping
-    public ResponseEntity<CourseTemplateDto>createCourseTemplate(){
-        return new ResponseEntity<>(HttpStatus.CREATED);
+    public ResponseEntity<CourseTemplateDto>createCourseTemplate(@RequestBody PostCourseTemplateDto dto){
+        CourseTemplate template=courseTemplateMappingService.mapPostCourseTemplateDtoToCourseTemplate(dto);
+        CourseTemplate data=service.createCourse(template);
+        CourseTemplateDto createdCourse=courseTemplateMappingService.mapCourseTemplateToCourseTemplateDto(data);
+        return new ResponseEntity<>(createdCourse,HttpStatus.CREATED);
     }
 }
