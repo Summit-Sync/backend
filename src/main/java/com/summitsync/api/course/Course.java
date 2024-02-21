1db0aa556ac9846acf3bfd73423b7afc0056307b
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-02-21 09:47:29 +0100

    Added Controllers

--- src/main/java/com/summitsync/api/course/Course.java
+++ src/main/java/com/summitsync/api/course/Course.java
@@ -11,6 +11,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
