f006606e22c70d5831d8f8470af8c24447f4f060
Enrico004 <134366696+Enrico004@users.noreply.github.com>
2024-02-21 11:06:31 +0100

    Course template and ModelMapper (#4)
    * Reorganized structure
    
    * Added Controllers
    
    * added data annotation
    
    * create ModelMapper and added Rest-Endpoint
    
    * fixed constructor error
    
    ---------
    
    Co-authored-by: Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
    Co-authored-by: jan-okke <72443291+jan-okke@users.noreply.github.com>

--- src/main/java/com/summitsync/api/coursetemplate/CourseTemplateController.java
+++ src/main/java/com/summitsync/api/coursetemplate/CourseTemplateController.java
@@ -1,4 +1,15 @@
 package com.summitsync.api.coursetemplate;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/coursetemplate")
 public class CourseTemplateController {
+
+    @PostMapping
+    public ResponseEntity<CourseTemplateDto>createCourseTemplate(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
---
+++ src/main/java/com/summitsync/api/coursetemplate/CourseTemplateDto.java
@@ -0,0 +1,11 @@
+package com.summitsync.api.coursetemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+public class CourseTemplateDto {
+}
