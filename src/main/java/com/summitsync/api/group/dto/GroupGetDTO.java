03355f124f22c6033d7312df678cbb5fb9c4e8b7
Marius Meschter <marius.meschter@univention.de>
2024-04-03 11:57:55 +0200

    fix(group): make group work properly with respective DTOs

--- src/main/java/com/summitsync/api/group/dto/GroupGetDTO.java
+++ src/main/java/com/summitsync/api/group/dto/GroupGetDTO.java
@@ -3,17 +3,19 @@ package com.summitsync.api.group.dto;
 import com.summitsync.api.grouptemplate.dto.GroupTemplateGetDTO;
 import com.summitsync.api.grouptemplate.dto.GroupTemplatePostDTO;
 import com.summitsync.api.qualification.Qualification;
+import com.summitsync.api.qualification.dto.QualificationDto;
 import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Set;
 
 @Data
 public class GroupGetDTO {
     private long id;
     private GroupTemplateGetDTO template;
     private String notes;
-    private List<Qualification> requiredQualifications;
+    private Set<QualificationDto> requiredQualifications;
     private BigDecimal pricePerParticipant;
     private Integer numberOfParticipants;
     private String description;
