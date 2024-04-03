db76a8dd1b415e02bf6c002d1fca3124f34309da
Marius Meschter <marius.meschter@univention.de>
2024-04-03 11:57:22 +0200

    fix(coursetemplateprice): add REST endpoints

---
+++ src/main/java/com/summitsync/api/coursetemplateprice/CourseTemplatePriceRepository.java
@@ -0,0 +1,6 @@
+package com.summitsync.api.coursetemplateprice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseTemplatePriceRepository extends JpaRepository<CourseTemplatePrice, Long> {
+}
