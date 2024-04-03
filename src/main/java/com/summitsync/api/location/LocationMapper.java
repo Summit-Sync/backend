4f319696223776a20265fc88c2f788f92b24c3f9
Enrico Wiegmann <enrico.wiegmann@ssi-schaefer.com>
2024-04-03 09:49:04 +0200

    location REST

---
+++ src/main/java/com/summitsync/api/location/LocationMapper.java
@@ -0,0 +1,44 @@
+package com.summitsync.api.location;
+
+import com.summitsync.api.location.dto.GetLocationDto;
+import com.summitsync.api.location.dto.PostLocationDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LocationMapper {
+
+    public Location mapPostLocationDtoToLocation(PostLocationDto dto){
+        return Location.builder()
+                .country(dto.getCountry())
+                .email(dto.getEmail())
+                .mapsUrl(dto.getMapsUrl())
+                .postCode(dto.getPostCode())
+                .phone(dto.getPhone())
+                .street(dto.getStreet())
+                .room(dto.getRoom()).build();
+    }
+
+    public GetLocationDto mapLocationToGetLocationDto(Location location){
+        return GetLocationDto.builder()
+                .locationId(location.getLocationId())
+                .country(location.getCountry())
+                .email(location.getEmail())
+                .mapsUrl(location.getMapsUrl())
+                .phone(location.getPhone())
+                .postCode(location.getPostCode())
+                .room(location.getRoom())
+                .street(location.getStreet()).build();
+    }
+
+    public Location mapLocationDtoToLocation(GetLocationDto dto){
+        return Location.builder()
+                .locationId(dto.getLocationId())
+                .country(dto.getCountry())
+                .email(dto.getEmail())
+                .mapsUrl(dto.getMapsUrl())
+                .postCode(dto.getPostCode())
+                .phone(dto.getPhone())
+                .street(dto.getStreet())
+                .room(dto.getRoom()).build();
+    }
+}
---
+++ src/main/java/com/summitsync/api/location/LocationService.java
@@ -0,0 +1,52 @@
+package com.summitsync.api.location;
+
+import com.summitsync.api.exceptionhandler.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository repository;
+
+    public Location getLocationById(Long id){
+        Optional<Location>data=repository.findById(id);
+        if(data.isEmpty()){
+            throw new ResourceNotFoundException("Location on id: "+id+" does not exist");
+        }
+        return data.get();
+    }
+
+    public List<Location>getAllLocations(){
+        return repository.findAll();
+    }
+
+    public Location updateLocation(Location location){
+        Location data=getLocationById(location.getLocationId());
+        data.setCountry(location.getCountry());
+        data.setPhone(location.getPhone());
+        data.setEmail(location.getEmail());
+        data.setRoom(location.getRoom());
+        data.setStreet(location.getStreet());
+        data.setMapsUrl(location.getMapsUrl());
+        data.setPostCode(location.getPostCode());
+        return repository.save(data);
+    }
+
+    public Location createLocation(Location location){
+        return repository.save(location);
+    }
+
+    public void deleteLocation(Long id){
+        if(repository.existsById(id)){
+            repository.deleteById(id);
+        } else{
+            throw new ResourceNotFoundException("Location on id: "+id+" does not exist");
+        }
+
+    }
+}
