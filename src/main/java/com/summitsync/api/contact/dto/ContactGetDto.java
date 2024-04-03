7b8f19b59226306bacd6471ec15bbca6d3085cb9
Marius Meschter <marius.meschter@univention.de>
2024-04-03 09:37:05 +0200

    fix(contact): add REST endpoints

---
+++ src/main/java/com/summitsync/api/contact/dto/ContactGetDto.java
@@ -0,0 +1,14 @@
+package com.summitsync.api.contact.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ContactGetDto {
+    private long contactId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String telephone;
+}
