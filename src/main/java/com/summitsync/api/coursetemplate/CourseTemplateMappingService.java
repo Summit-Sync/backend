ce4511ba2aa9bbae9f2e6a84f546f83d4db803dd
Marius Meschter <marius.meschter@univention.de>
2024-04-03 11:56:51 +0200

    fix(coursetemplate): make proper use of DTOs

--- src/main/java/com/summitsync/api/coursetemplate/CourseTemplateMappingService.java
+++ src/main/java/com/summitsync/api/coursetemplate/CourseTemplateMappingService.java
@@ -2,59 +2,59 @@ package com.summitsync.api.coursetemplate;
 
 import com.summitsync.api.coursetemplate.dto.CourseTemplateDto;
 import com.summitsync.api.coursetemplate.dto.PostCourseTemplateDto;
-import com.summitsync.api.coursetemplate.dto.UpdateCourseTemplateDto;
-import com.summitsync.api.qualification.Qualification;
-import com.summitsync.api.qualification.QualificationService;
+import com.summitsync.api.coursetemplateprice.CourseTemplatePriceMapper;
+import com.summitsync.api.qualification.QualificationMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class CourseTemplateMappingService {
 
-    private final QualificationService qualificationService;
+    private final CourseTemplatePriceMapper courseTemplatePriceMapper;
+    private final QualificationMapper qualificationMapper;
 
     public CourseTemplate mapPostCourseTemplateDtoToCourseTemplate(PostCourseTemplateDto dto){
-        List<Qualification>qualificationList = new ArrayList<>();
-        for(Qualification qualification : dto.getQualificationList()){
-            qualificationList.add(this.qualificationService.findById(qualification.getQualificationId()));
-        }
-        return new CourseTemplate(dto.getAcronym(), dto.getTitle(),dto.getNumberOfDates(),dto.getDescription(),qualificationList,dto.getNumberOfParticipants(), dto.getNumberOfWaitList(), dto.getNumberOfTrainers(),dto.getPriceList(),dto.getDuration(), dto.getNumberOfMinutesPerDate());
+        return new CourseTemplate(
+                dto.getAcronym(),
+                dto.getTitle(),
+                dto.getNumberOfDates(),
+                dto.getDescription(),
+                new HashSet<>(),
+                dto.getNumberOfParticipants(),
+                dto.getNumberOfWaitList(),
+                dto.getNumberOfTrainers(),
+                new HashSet<>(),
+                dto.getDuration(),
+                dto.getNumberOfMinutesPerDate()
+        );
     }
 
-    public CourseTemplateDto mapCourseTemplateToCourseTemplateDto(CourseTemplate data){
+    public CourseTemplateDto mapCourseTemplateToCourseTemplateDto(CourseTemplate courseTemplate){
+        var priceListSet = courseTemplate.getPriceList()
+                .stream()
+                .map(this.courseTemplatePriceMapper::mapCourseTemplatePriceToCourseTemplatePriceDto)
+                .collect(Collectors.toSet());
+
+        var qualificationSet = courseTemplate.getRequiredQualifications()
+                .stream()
+                .map(this.qualificationMapper::mapQualificationToQualificationDto)
+                .collect(Collectors.toSet());
+
         return CourseTemplateDto.builder()
-                .id(data.getBaseTemplateId())
-                .title(data.getTitle())
-                .numberOfParticipants(data.getNumberOfParticipants())
-                .numberOfTrainers(data.getNumberOfTrainers())
-                .numberOfWaitList(data.getNumberOfWaitList())
-                .priceList(data.getPriceList())
-                .acronym(data.getAcronym())
-                .description(data.getDescription())
-                .numberOfDates(data.getNumberOfDates())
-                .qualificationList(data.getRequiredQualifications())
+                .id(courseTemplate.getBaseTemplateId())
+                .title(courseTemplate.getTitle())
+                .numberOfParticipants(courseTemplate.getNumberOfParticipants())
+                .numberOfTrainers(courseTemplate.getNumberOfTrainers())
+                .numberOfWaitList(courseTemplate.getNumberOfWaitList())
+                .priceList(priceListSet)
+                .acronym(courseTemplate.getAcronym())
+                .description(courseTemplate.getDescription())
+                .numberOfDates(courseTemplate.getNumberOfDates())
+                .qualificationList(qualificationSet)
                 .build();
-    }
-
-    public CourseTemplate mapUpdateCourseTemplateDtoToCourseTemplate(UpdateCourseTemplateDto dto){
-        List<Qualification>qualificationList = new ArrayList<>();
-        for(Qualification qualification : dto.getQualificationList()){
-            qualificationList.add(this.qualificationService.findById(qualification.getQualificationId()));
-        }
-        return new CourseTemplate(dto.getAcronym(), dto.getTitle(),dto.getNumberOfDates(),dto.getDescription(),qualificationList,dto.getNumberOfParticipants(), dto.getNumberOfWaitList(), dto.getNumberOfTrainers(),dto.getPriceList(), dto.getDuration(), dto.getNumberOfMinutesPerDate());
-    }
-
-    public CourseTemplate mapGetCourseTemplateDtoToCourseTemplate(CourseTemplateDto dto) {
-        List<Qualification>qualificationList = new ArrayList<>();
-        for(Qualification qualification : dto.getQualificationList()){
-            qualificationList.add(this.qualificationService.findById(qualification.getQualificationId()));
-        }
-        CourseTemplate newTemplate = new CourseTemplate(dto.getAcronym(), dto.getTitle(),dto.getNumberOfDates(),dto.getDescription(),qualificationList,dto.getNumberOfParticipants(), dto.getNumberOfWaitList(), dto.getNumberOfTrainers(),dto.getPriceList(),dto.getDuration(), dto.getNumberOfMinutesPerDate());
-        newTemplate.setBaseTemplateId(dto.getId());
-        return newTemplate;
     }
 }
