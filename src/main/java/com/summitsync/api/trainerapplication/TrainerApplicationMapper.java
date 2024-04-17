1f1d45cdf51fb30a58f29f96083eaef4b4495efc
jan-okke <jan1263@schule.bremen.de>
2024-04-17 10:51:52 +0200

    TrainerApplicationController

---
+++ src/main/java/com/summitsync/api/trainerapplication/TrainerApplicationMapper.java
@@ -0,0 +1,26 @@
+package com.summitsync.api.trainerapplication;
+
+import com.summitsync.api.trainer.TrainerMapper;
+import com.summitsync.api.trainerapplication.dto.TrainerApplicationDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TrainerApplicationMapper {
+    private final TrainerMapper trainerMapper;
+    public TrainerApplicationDto mapTrainerApplicationToTrainerApplicationDto(TrainerApplication application, JwtAuthenticationToken jwt) {
+        TrainerApplicationDto dto = new TrainerApplicationDto();
+        dto.setId(application.getApplicationId());
+        var trainerDto = trainerMapper.mapTrainerToTrainerDto(application.getTrainer(), jwt.getToken().getTokenValue());
+        dto.setPhone(trainerDto.getPhone());
+        dto.setSubjectId(trainerDto.getSubjectId());
+        dto.setQualificationDTOs(trainerDto.getQualifications());
+        dto.setLastName(trainerDto.getLastName());
+        dto.setFirstName(trainerDto.getFirstName());
+        dto.setAccepted(application.isAccepted());
+        dto.setEmail(trainerDto.getEmail());
+        return dto;
+    }
+}
---
+++ src/main/java/com/summitsync/api/trainerapplication/TrainerApplicationRepository.java
@@ -0,0 +1,6 @@
+package com.summitsync.api.trainerapplication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainerApplicationRepository extends JpaRepository<TrainerApplication, Long> {
+}
