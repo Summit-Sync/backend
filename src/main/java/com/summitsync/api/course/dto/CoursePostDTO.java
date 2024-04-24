9739a1f79afe741cdc294faea4fdd7d3661695e8
jan-okke <jan1263@schule.bremen.de>
2024-04-24 08:55:18 +0200

    CoursePostDTO Validation

--- src/main/java/com/summitsync/api/course/dto/CoursePostDTO.java
+++ src/main/java/com/summitsync/api/course/dto/CoursePostDTO.java
@@ -7,6 +7,7 @@ import com.summitsync.api.location.Location;
 import com.summitsync.api.participant.Participant;
 import com.summitsync.api.qualification.Qualification;
 import com.summitsync.api.qualification.dto.QualificationDto;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
@@ -17,19 +18,32 @@ import java.util.List;
 @Data
 @Builder
 public class CoursePostDTO {
+    @NotBlank(message = "Visible cannot be empty.")
     private boolean visible;
+    @NotBlank(message = "Acronym cannot be empty.")
     private String acronym;
+    @NotBlank(message = "Description cannot be empty.")
     private String description;
+    @NotBlank(message = "Dates cannot be empty.")
     private List<LocalDateTime> dates;
+    @NotBlank(message = "Duration cannot be empty.")
     private int duration;
+    @NotBlank(message = "Number of Participants cannot be empty.")
     private int numberParticipants;
+    @NotBlank(message = "Number of Waitlist cannot be empty.")
     private int numberWaitlist;
+    @NotBlank(message = "Prices cannot be empty.")
     private List<Long> prices;
+    @NotBlank(message = "Location cannot be empty.")
     private long location;
+    @NotBlank(message = "Meeting Point cannot be empty.")
     private String meetingPoint;
+    @NotBlank(message = "Required Qualifications cannot be empty.")
     private List<Long> requiredQualifications;
+    @NotBlank(message = "Number of Trainers cannot be empty.")
     private int numberTrainers;
     private String notes;
+    @NotBlank(message = "Title cannot be empty.")
     private String title;
 }
 
