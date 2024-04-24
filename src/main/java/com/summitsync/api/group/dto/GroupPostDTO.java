61391cd945a9c6f28fc374b704e2e66342239bd8
jan-okke <jan1263@schule.bremen.de>
2024-04-24 09:26:42 +0200

    Group

--- src/main/java/com/summitsync/api/group/dto/GroupPostDTO.java
+++ src/main/java/com/summitsync/api/group/dto/GroupPostDTO.java
@@ -1,5 +1,7 @@
 package com.summitsync.api.group.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -9,19 +11,39 @@ import java.util.Set;
 
 @Data
 public class GroupPostDTO {
+    @NotBlank(message = "Title cannot be empty.")
     String title;
+    @NotBlank(message = "Description cannot be empty.")
     String description;
+    @NotBlank(message = "Number of Dates cannot be empty.")
+    @PositiveOrZero(message = "Number of Dates must be positive or zero.")
     int numberOfDates;
+    @NotBlank(message = "Dates cannot be empty.")
     List<LocalDateTime> dates;
+    @NotBlank(message = "Duration cannot be empty.")
+    @PositiveOrZero(message = "Duration must be positive or zero.")
     int duration;
+    @NotBlank(message = "Number of participants cannot be empty.")
+    @PositiveOrZero(message = "Number of participants must be positive or zero.")
     int numberParticipants;
+    @NotBlank(message = "Contact cannot be empty.")
     long contact;
+    @NotBlank(message = "Location cannot be empty.")
     long location;
+    @NotBlank(message = "Meeting Point cannot be empty.")
     String meetingPoint;
+    @NotBlank(message = "Trainer Price per Hour cannot be empty.")
+    @PositiveOrZero(message = "Trainer Price per hour must be positive or zero.")
     BigDecimal trainerPricePerHour;
+    @NotBlank(message = "Price per Participant cannot be empty.")
+    @PositiveOrZero(message = "Price per Participant must be positive or zero.")
     BigDecimal pricePerParticipant;
+    @NotBlank(message = "Required qualifications cannot be empty.")
     Set<Integer> requiredQualifications;
+    @NotBlank(message = "Participants per Trainer cannot be empty.")
+    @PositiveOrZero(message = "Participants per Trainer must be positive or zero.")
     int participantsPerTrainer;
+    @NotBlank(message = "Trainers cannot be empty.")
     Set<Integer> trainers;
 
 }
