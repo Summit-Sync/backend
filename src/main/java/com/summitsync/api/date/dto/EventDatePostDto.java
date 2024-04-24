7c7a1c29657300afe2a233849a5b1cdaf5a78767
jan-okke <jan1263@schule.bremen.de>
2024-04-24 09:20:08 +0200

    Date

--- src/main/java/com/summitsync/api/date/dto/EventDatePostDto.java
+++ src/main/java/com/summitsync/api/date/dto/EventDatePostDto.java
@@ -1,11 +1,16 @@
 package com.summitsync.api.date.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
 public class EventDatePostDto {
+    @NotBlank(message = "Start Time cannot be empty.")
     private LocalDateTime startTime;
+    @NotBlank(message = "Duration in minutes cannot be empty.")
+    @PositiveOrZero(message = "The duration must be positive or zero.")
     private int durationInMinutes;
 }
