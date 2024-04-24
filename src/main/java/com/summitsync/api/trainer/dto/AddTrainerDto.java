1bff2bb90046935c79a1b2a865e8eb84716b6999
Marius Meschter <marius.meschter@univention.de>
2024-04-24 09:32:45 +0200

    Trainer validaion

--- src/main/java/com/summitsync/api/trainer/dto/AddTrainerDto.java
+++ src/main/java/com/summitsync/api/trainer/dto/AddTrainerDto.java
@@ -1,13 +1,19 @@
 package com.summitsync.api.trainer.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AddTrainerDto {
+    @NotBlank(message = "username can not be empty")
     String username;
+    @NotBlank(message = "firstname can not be empty")
     String firstName;
+    @NotBlank(message = "lastname can not be empty")
     String lastName;
+    @NotBlank(message = "password can not be empty")
     String password;
+    @NotBlank(message = "email can not be empty")
     String email;
     String phone;
 }
