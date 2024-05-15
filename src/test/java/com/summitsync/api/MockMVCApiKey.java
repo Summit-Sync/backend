e7f3a51ce30058a1522ee5db5e4faf0ece7dc9f6
Marius Meschter <marius.meschter@univention.de>
2024-05-15 12:56:26 +0200

    test: add test for course crud

--- src/test/java/com/summitsync/api/MockMVCApiKey.java
+++ src/test/java/com/summitsync/api/MockMVCApiKey.java
@@ -11,8 +11,7 @@ import java.util.HashMap;
 
 @Component
 public class MockMVCApiKey implements MockMvcBuilderCustomizer {
-    @Override
-    public void customize(ConfigurableMockMvcBuilder<?> builder) {
+    public static String getAccessToken() {
         var restClient = RestClient.create();
         var res = restClient.post()
                 .uri("https://keycloak.summitsync.meschter.me/realms/summit-sync/protocol/openid-connect/token")
@@ -21,9 +20,14 @@ public class MockMVCApiKey implements MockMvcBuilderCustomizer {
                 .retrieve()
                 .body(HashMap.class);
 
+        return (String) res.get("access_token");
+    }
+    @Override
+    public void customize(ConfigurableMockMvcBuilder<?> builder) {
+
         var apiKeyRequestBuilder = MockMvcRequestBuilders
                 .get("/")
-                .header("Authorization", "Bearer " + res.get("access_token"));
+                .header("Authorization", "Bearer " + MockMVCApiKey.getAccessToken());
 
         builder.defaultRequest(apiKeyRequestBuilder);
     }
---
+++ src/test/java/com/summitsync/api/integrationtest/course/CourseCRUDTest.java
@@ -0,0 +1,212 @@
+package com.summitsync.api.integrationtest.course;
+
+import com.summitsync.api.MockMVCApiKey;
+import com.summitsync.api.integrationtest.testcontainers.AbstractIntegrationTest;
+import com.summitsync.api.location.Location;
+import com.summitsync.api.location.LocationService;
+import com.summitsync.api.price.Price;
+import com.summitsync.api.price.PriceService;
+import com.summitsync.api.qualification.Qualification;
+import com.summitsync.api.qualification.QualificationService;
+import com.summitsync.api.trainer.TrainerService;
+import com.summitsync.api.trainer.dto.AddTrainerDto;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.math.BigDecimal;
+import java.util.Random;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CourseCRUDTest extends AbstractIntegrationTest {
+    @Autowired
+    private QualificationService qualificationService;
+    @Autowired
+    private LocationService locationService;
+    @Autowired
+    private TrainerService trainerService;
+    private Random random = new Random();
+    @Autowired
+    private PriceService priceService;
+
+    @BeforeEach
+    void setup() {
+        var testQuali1 = Qualification.builder().name("Erste Hilfe").build();
+        var testQuali2 = Qualification.builder().name("Zweite Hilfe").build();
+        this.qualificationService.saveQualification(testQuali1);
+        this.qualificationService.saveQualification(testQuali2);
+        var location = Location.builder().country("Germany").phone("+491256321").street("Straße 1").build();
+        this.locationService.createLocation(location);
+        var jwt = MockMVCApiKey.getAccessToken();
+        var trainer1 = AddTrainerDto.builder()
+                .username("it_test_trainer" + random.nextInt(5000))
+                .firstName("Integration")
+                .lastName("Test")
+                .password("test")
+                .email("test@test.test" + random.nextInt(5000))
+                .phone("+41241615")
+                .build();
+        this.trainerService.newTrainer(trainer1, jwt);
+        var price1 = Price.builder()
+                .price(BigDecimal.valueOf(10.0))
+                .name("Test Price 1")
+                .build();
+
+        priceService.create(price1);
+    }
+
+    @Test
+    @Order(1)
+    void testCreateCourseHappyPath() throws Exception {
+        var content = """
+{
+   "acronym":"kv",
+   "dates":[
+      "2024-05-13T10:00:25.739Z",
+      "2024-05-13T10:00:40.456Z",
+      "2024-08-01T09:00:00.000Z"
+   ],
+   "description":"kvBeschreibrung",
+   "duration":40,
+   "location":1,
+   "meetingPoint":"kvTreffpunkt",
+   "notes":"test",
+   "numberParticipants":2,
+   "numberTrainers":2,
+   "numberWaitlist":2,
+   "prices":[
+      1
+   ],
+   "requiredQualifications":[
+      1,
+      2
+   ],
+   "title":"kvTitle",
+   "visible":false,
+   "notes": "test"
+}
+""";
+        this.mockMvc.perform(post("/api/v1/course")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("visible").value(false))
+                .andExpect(jsonPath("dates.length()").value(3))
+                .andExpect(jsonPath("prices[0].name").value("Test Price 1"))
+                .andExpect(jsonPath("location.country").value("Germany"))
+                .andExpect(jsonPath("notes").value("test"));
+
+    }
+    @Test
+    @Order(2)
+    void testAddTrainerToCourse() throws Exception {
+        var content = """
+[1]
+""";
+        this.mockMvc.perform(put("/api/v1//course/1/trainer")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(content))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("trainers[0].firstName").value("Integration"))
+                .andExpect(jsonPath("trainers.length()").value(1));
+    }
+
+    @Test
+    @Order(3)
+    void testUpdateCourse() throws Exception {
+        var content = """
+{
+   "acronym":"kv",
+   "dates":[
+      "2024-05-13T10:00:25.739Z",
+      "2024-05-13T10:00:40.456Z",
+      "2024-08-01T09:00:00.000Z"
+   ],
+   "description":"kvBeschreibrung",
+   "duration":35,
+   "location":1,
+   "meetingPoint":"kvTreffpunkt",
+   "notes":"test updated",
+   "numberParticipants":2,
+   "numberTrainers":2,
+   "numberWaitlist":2,
+   "prices":[
+      1
+   ],
+   "requiredQualifications":[
+      1,
+      2
+   ],
+   "title":"kvTitle",
+   "visible":false
+}
+""";
+        this.mockMvc.perform(put("/api/v1/course/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("id").value(1))
+                .andExpect(jsonPath("visible").value(false))
+                .andExpect(jsonPath("canceled").value(false))
+                .andExpect(jsonPath("dates.length()").value(3))
+                .andExpect(jsonPath("prices[0].name").value("Test Price 1"))
+                .andExpect(jsonPath("location.country").value("Germany"))
+                .andExpect(jsonPath("notes").value("test updated"))
+                .andExpect(jsonPath("duration").value(35))
+                .andExpect(jsonPath("trainers[0].firstName").value("Integration"))
+                .andExpect(jsonPath("trainers.length()").value(1));
+    }
+
+    @Test
+    @Order(4)
+    void testCreateSecondCourseHappyPath() throws Exception {
+        var content = """
+{
+   "acronym":"kv2",
+   "dates":[
+      "2024-05-13T10:00:25.739Z",
+      "2024-05-13T10:00:40.456Z",
+      "2024-08-01T09:00:00.000Z"
+   ],
+   "description":"kvBeschreibrung",
+   "duration":40,
+   "location":1,
+   "meetingPoint":"kvTreffpunkt",
+   "notes":"test",
+   "numberParticipants":2,
+   "numberTrainers":2,
+   "numberWaitlist":2,
+   "prices":[
+      1
+   ],
+   "requiredQualifications":[
+      1,
+      2
+   ],
+   "title":"kvTitle",
+   "visible":false,
+   "notes": "test"
+}
+""";
+        this.mockMvc.perform(post("/api/v1/course")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("id").value(2))
+                .andExpect(jsonPath("acronym").value("kv2"))
+                .andExpect(jsonPath("visible").value(false))
+                .andExpect(jsonPath("dates.length()").value(3))
+                .andExpect(jsonPath("prices[0].name").value("Test Price 1"))
+                .andExpect(jsonPath("location.country").value("Germany"))
+                .andExpect(jsonPath("notes").value("test"));
+
+    }
+}
